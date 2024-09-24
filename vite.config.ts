ce8e1843da8d6516e837b9dbc70e2c85e9ddb2fa
Rishabh Gusain <rishabhgusain.6@gmail.com>
2024-09-24 22:28:23 +0530

    comit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
+import path from 'path';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-import { defineConfig } from "vite"
- 
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react(),eslint()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
