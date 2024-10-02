553d05e22f29f178b4525ddf26919bdd8387020e
Rishabh Gusain <rishabhgusain.6@gmail.com>
2024-10-02 12:12:56 +0530

    corrected url slash

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -150,7 +150,7 @@ export default function Home() {
             console.log('Running route..'+ `${import.meta.env.VITE_API_URL}/calculate`);
             const response = await axios({
                 method: 'post',
-                url: `${import.meta.env.VITE_API_URL}/calculate`,
+                url: `${import.meta.env.VITE_API_URL}calculate`,
                 data: {
                     image: canvas.toDataURL('image/png'),
                     dict_of_vars: dictOfVars
