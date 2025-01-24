0969b9143b72858efee676384d02b01a69ebb5ef
r69 <143521130+r69shabh@users.noreply.github.com>
2025-01-24 17:01:59 +0530

    Delete src directory

--- src/screens/auth/logout.tsx
+++
@@ -1,6 +0,0 @@
-// src/utils/auth.ts
-
-export const logout = () => {
-  localStorage.removeItem('authToken');
-  window.location.href = '/login'; // Redirect to login page
-};
--- src/types/vite-plugin-eslint.d.ts
+++
@@ -1,5 +0,0 @@
-// vite-plugin-eslint.d.ts
-declare module 'vite-plugin-eslint' {
-    const eslint: any;
-    export default eslint;
-  }
